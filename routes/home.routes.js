05a9218cb1592cb520d84b8773d1dee52cf0a05f
Mariana Rascon <nienorloth@gmail.com>
2020-07-26 14:30:31 -0500

    ading routes

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const { HomeController } = require('../controllers');
 
 router.get('/', HomeController.index);
+router.get('/about', HomeController.about);
+router.get('/quotes', HomeController.quotes);
 
 module.exports = router;
