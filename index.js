8b341b3b20b7fadbd953a141972327ab1ab5db28
Mariana Rascon <nienorloth@gmail.com>
2020-07-26 13:48:59 -0500

    adding MVC

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const server = express();
+const { PORT } = require('./config');
+const { HomeRoutes } = require('./routes');
+
+server.use(express.static('./public'));
+server.use(express.json());
+
+server.use('/', HomeRoutes);
+
+server.listen(PORT, () => {
+    console.log(`Application running on PORT ${PORT}`)
+});
